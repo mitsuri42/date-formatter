bffb0443a5f54e5af97264850fbc9731db8404d4
Dmitriy Pysarchuk <dmit.pysarchuk@gmail.com>
2024-02-02 13:30:32 +0200

    Add formation of date

--- script.js
+++ script.js
@@ -5,4 +5,9 @@ const date = new Date();
 const day = date.getDate();
 const month = date.getMonth() + 1;
 const year = date.getFullYear();
+const hours = date.getHours();
+const minutes = date.getMinutes();
+
+const formattedDate = `${day}-${month}-${year}`;
+currentDateParagraph.textContent = formattedDate;
 
